7736a0093496ad3393dc27b2e05f8c7dd5c76e4f
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-23 14:26:09 -0300

    Routes mapping

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -6,8 +6,10 @@ const express = require("express");
 const server = express();
 
 server.use(bodyParser, urlencoded({ extended: true }));
-server.use(bodyParser.json())
+server.use(bodyParser.json());
 
-server.listen(port, function(){
-    console.log(`BACKEND is running on port ${port}`)
-})
+server.listen(port, function () {
+  console.log(`BACKEND is running on port ${port}`);
+});
+
+module.exports = server;

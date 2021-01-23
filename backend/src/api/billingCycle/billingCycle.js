ae37b771a443b0061046b40088a0ae367230b99e
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-23 15:12:46 -0300

    Testing API

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -1,6 +1,8 @@
 const restful = require("node-restful");
 const mongoose = restful.mongoose;
 
+const STATUS = ["PAGO", "PENDENTE", "AGENDADO"];
+
 const creditSchema = new mongoose.Schema({
   name: { type: String, required: true },
   value: { type: Number, min: 0, required: true },
@@ -13,7 +15,7 @@ const debitSchema = new mongoose.Schema({
     type: String,
     required: false,
     uppercase: true,
-    enum: ["PAGO", "PENDENTE", "AGENDADO"],
+    enum: STATUS,
   },
 });
 
@@ -21,8 +23,8 @@ const billingCycleSchema = new mongoose.Schema({
   name: { type: String, required: true },
   month: { type: Number, min: 1, max: 12, required: true },
   year: { type: Number, min: 1970, max: 2100, required: true },
-  credits: { creditSchema },
-  debits: { debitSchema },
+  credits: [creditSchema],
+  debits: [debitSchema],
 });
 
-module.exports = restful.model('BillingCycle', billingCycleSchema)
+module.exports = restful.model("BillingCycle", billingCycleSchema);

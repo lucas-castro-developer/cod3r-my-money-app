92ad0fc2c649c31d0fce25e5c607debfff07b6bb
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-23 20:16:12 -0300

    Enabling CORS

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,6 +4,7 @@ const { urlencoded } = require("body-parser");
 const bodyParser = require("body-parser");
 const express = require("express");
 const server = express();
+const allowCors = require("./cors");
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());

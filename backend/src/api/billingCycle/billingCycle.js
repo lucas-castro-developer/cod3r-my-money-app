db0da2178ffe49522f4d68c078e6adf09139e9bf
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-23 14:33:51 -0300

    Correct uppercase attribute value

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -12,7 +12,7 @@ const debitSchema = new mongoose.Schema({
   status: {
     type: String,
     required: false,
-    uppercase,
+    uppercase: true,
     enum: ["PAGO", "PENDENTE", "AGENDADO"],
   },
 });

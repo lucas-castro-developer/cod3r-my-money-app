06a4a9b067a34bb19a9584ef566f1e8d5368717d
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-23 19:51:07 -0300

    Getting summary information

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -29,25 +29,25 @@ BillingCycle.route("summary", (req, res, next) => {
       {
         $project: {
           credit: { $sum: "$credits.value" },
-          debt: { $sum: "$debts.value" },
+          debit: { $sum: "$debits.value" },
         },
       },
       {
         $group: {
           _id: null,
           credit: { $sum: "$credit" },
-          debt: { $sum: "$debt" },
+          debit: { $sum: "$debit" },
         },
       },
       {
-        $project: { _id: 0, credit: 1, debt: 1 },
+        $project: { _id: 0, credit: 1, debit: 1 },
       },
     ],
     (error, result) => {
       if (error) {
         res.status(500).json({ errors: [error] });
       } else {
-        res.json(result[0] || { credit: 0, debt: 0 });
+        res.json(result[0] || { credit: 0, debit: 0 });
       }
     }
   );

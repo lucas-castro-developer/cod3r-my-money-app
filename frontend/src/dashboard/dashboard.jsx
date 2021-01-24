3a4aced539b9eb1b79d96c2ccfa530b2a8fb8fa0
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-24 17:37:51 -0300

    Using valuebox

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -2,16 +2,42 @@ import React, { Component } from "react";
 
 import Content from "../common/template/content";
 import ContentHeader from "../common/template/contentHeader";
+import ValueBox from "../common/widget/valueBox";
+import Row from "../common/layout/row";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <ContentHeader title="Dahsboard" small="Versão 1.0" />
-        <Content>Dashboard</Content>
+        <Content>
+          <Row>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value="R$ 10,00"
+              text="Total de Créditos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value="R$ 10,00"
+              text="Total de Débitos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value="R$ 0,00"
+              text="Valor consolidado"
+            />
+          </Row>
+        </Content>
       </div>
     );
   }
 }
 
-export default Dashboard
+export default Dashboard;

8fe6ff72bc00adc64dd5c0d8c93e5ccb3c004a1b
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-24 15:01:51 -0300

    Footer component

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2021
+            || <a href='https://www.linkedin.com/in/lucas-castro-5762a6125/'>Lucas dos Santos Castro</a>.
+        </strong>
+    </footer>
+)

045aee5927c54fb37fb4d3f6c794570bb289444c
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-25 20:10:28 -0300

    Generalizing CreditList components

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,7 +5,7 @@ import { reduxForm, Field, formValueSelector } from "redux-form";
 
 import { init } from "./billingCycleActions";
 import LabelAndInput from "../common/form/labelAndInput";
-import CreditList from "./creditList";
+import ItemList from "./itemList";
 
 class BillingCycleForm extends Component {
   render() {
@@ -39,7 +39,13 @@ class BillingCycleForm extends Component {
             placeholder="Informe o ano"
             readOnly={readOnly}
           />
-          <CreditList cols="12 6" readOnly={readOnly} list={credits} />
+          <ItemList
+            cols="12 6"
+            readOnly={readOnly}
+            list={credits}
+            field="credits"
+            legend="Créditos"
+          />
         </div>
         <div className="box-footer">
           <button type="submit" className={`btn btn-${this.props.submitClass}`}>

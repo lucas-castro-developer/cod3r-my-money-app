b89ea14dcfb2c74311b4ad71d0f49f9a8d35d232
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-25 13:02:49 -0300

    Improving creation of billingCycle

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { toastr } from "react-redux-toastr";
 const BASE_URL = "http://localhost:3003/api";
+import { reset as resetForm } from "redux-form";
+import { showTabs, selectTab } from "../common/tab/tabActions";
 
 export function getList() {
   const request = axios.get(`${BASE_URL}/billingCycles`);
@@ -11,15 +13,23 @@ export function getList() {
 }
 
 export function create(values) {
-  axios
-    .post(`${BASE_URL}/billingCycles`, values)
-    .then((resp) => {
-      toastr.success("Sucesso", "Operação Realizada com sucesso.");
-    })
-    .catch((e) => {
-      e.response.data.errors.forEach((error) => toastr.error("Erro", error));
-    });
-  return {
-    type: "TEMP",
+  return (dispatch) => {
+    axios
+      .post(`${BASE_URL}/billingCycles`, values)
+      .then((resp) => {
+        toastr.success("Sucesso", "Operação Realizada com sucesso.");
+        dispatch([
+          resetForm("billingCycleForm"),
+          getList(),
+          selectTab("tabList"),
+          showTabs("tabList", "tabCreate"),
+        ]);
+      })
+      .catch((e) => {
+        e.response.data.errors.forEach((error) => toastr.error("Erro", error));
+      });
+    return {
+      type: "TEMP",
+    };
   };
 }

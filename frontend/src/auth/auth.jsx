735390a661e5fb38f2a186ee9beedab3d3da4868
Lucas Castro <lucas.sc98@hotmail.com>
2021-03-29 19:13:13 -0300

    Authentication - Frontend - #06

--- frontend/src/auth/auth.jsx
+++ frontend/src/auth/auth.jsx
@@ -6,9 +6,9 @@ import { bindActionCreators } from "redux";
 import { login, signup } from "./authActions";
 import Row from "../common/layout/row";
 import Grid from "../common/layout/grid";
-import If from "../common/operator/if";
 import Messages from "../common/msg/messages";
 import Input from "../common/form/inputAuth";
+
 class Auth extends Component {
   constructor(props) {
     super(props);
